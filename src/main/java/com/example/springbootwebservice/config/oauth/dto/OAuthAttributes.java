fc5bf0ac578ac697257825dbe5b6905d94bca5f0
zsoon96 <pres0616@gmail.com>
2022-07-25 21:06:42 +0900

    네이버 로그인 시큐리티 설정_SerializationFailedException

--- src/main/java/com/example/springbootwebservice/config/oauth/dto/OAuthAttributes.java
+++ src/main/java/com/example/springbootwebservice/config/oauth/dto/OAuthAttributes.java
@@ -27,6 +27,11 @@ public class OAuthAttributes {
 
     // OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해줘야 함
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        // Naver 로그인 설정
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -47,6 +52,18 @@ public class OAuthAttributes {
                 .email(email)
                 .picture(picture)
                 .role(Role.GUEST) // 가입할 때 기본권한을 GUEST로 설정
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String)response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
                 .build();
     }
 }
